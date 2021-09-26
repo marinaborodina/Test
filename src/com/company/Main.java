267a926060fb2a3bc2dcd86475fbfa7ad6890385
marinaborodina <mborodina@tsn.at>
2021-09-26 15:24:07 +0200

    new feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Git remote test2");
+        System.out.println("Git remote merge test");
+        System.out.println("New feature");
     }
 }
